51433978ca89ee77a7abf377438b01e1856cb6e4
RagurivonRaguri <lincolnmuraguri93@gmail.com>
2023-04-02 22:39:32 +0300

    Done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # code-challenge3
+# Flatdango Movie House
+The deliverables are related to a movie ticket booking application. As a user of this application, I can see movie details of the first movie such as its poster, title, runtime, showtime, and available tickets when the page loads. The number of tickets available is calculated by subtracting the number of tickets sold from the total capacity of the theater. To retrieve the film data, a GET request needs to be made to the endpoint '/films/1'. The example response given shows the JSON format of the data retrieved.
+
+Additionally, I can also see a menu of all movies on the left side of the page in the 'ul#films' element when the page loads. This list can be styled by adding the classes 'film item' to each 'li' element. There is a hardcoded placeholder 'li' element in the 'ul#films' element, which can be removed using JavaScript or directly editing the HTML file. To retrieve film data for this list, a GET request to the endpoint '/films' needs to be made. The example response shows an array of JSON objects containing movie details for each movie.
+
+Lastly, as a user, I can buy a ticket for a movie by clicking the 'Buy Ticket' button. Upon clicking, the number of available tickets should decrease on the frontend. However, if there are no available tickets for the show, I should not be able to buy a ticket. This ensures that tickets are not oversold, and users do not face any inconvenience.
+# Author:Lincoln Muraguri
+# year[2023] author[L.M.M]
---
+++ script.js
@@ -0,0 +1,64 @@
+fetch('http://localhost:3000/films')
+  .then(res => res.json())
+  .then(data => movieList(data));
+
+function movieList(films) {
+  const listOfMovies = document.querySelector('#list-container');
+  films.forEach(film => {
+    const movie = document.createElement('li');
+    movie.innerHTML = `
+      <div class="card-body">
+        <h2>${film.title}</h2>
+      </div>
+    `;
+    listOfMovies.appendChild(movie);
+
+    movie.addEventListener('click', e => {
+      e.preventDefault();
+
+      const movieContainer = document.querySelector('#movie-container');
+      movieContainer.innerHTML = '';
+
+      const movieCard = document.createElement('li');
+      movieCard.setAttribute('id', film.id);
+      movieCard.innerHTML = `
+        <div id="card">
+          <h1 id="title">${film.title}</h1>
+          <img class="card-img" src="${film.poster}">
+          <div id="movieDetails">
+            <p>Total time: ${film.runtime} minutes</p>
+            <p>Showtime: ${film.showtime}</p>
+            <p>About: ${film.description}</p>
+            <p>Capacity: ${film.capacity}</p>
+            <p>Tickets sold: ${film.tickets_sold}</p>
+            <button id="btn">Buy Ticket</button>
+          </div>
+        </div>
+      `;
+      movieContainer.appendChild(movieCard);
+      const button = movieCard.querySelector('#btn');
+         button.addEventListener('click', () => {
+           if (film.tickets_sold < film.capacity) {
+           film.tickets_sold += 1;
+           const soldTickets = movieCard.querySelector('#movieDetails p:nth-of-type(2)');
+           soldTickets.textContent = `Tickets sold: ${film.tickets_sold}`;
+
+           const remainingTickets = document.createElement('p');
+          remainingTickets.textContent = `Remaining tickets: ${film.capacity - film.tickets_sold}`;
+            movieCard.querySelector('#movieDetails').appendChild(remainingTickets);
+
+    fetch(`http://localhost:3000/films/${film.id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ tickets_sold: film.tickets_sold })
+       })
+       .then(res => res.json())
+        } else {
+           alert('Sorry, Fully Booked');
+        }
+     }); 
+    });
+  });
+}
+
+
